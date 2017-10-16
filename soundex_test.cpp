1a775230afcc14e2a6a20487d98a18d36d71ead9
terminalobject <doctorboredom@hotmail.com>
2017-10-16 23:00:09 +0100

    separation of header

--- soundex_test.cpp
+++ soundex_test.cpp
@@ -1,31 +1,22 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-using ::testing::Eq;
+#include "Soundex.h"
 
-class Soundex {
+using namespace testing;
+
+class SoundexEncoding: public testing::Test {
 public:
-    std::string encode(const std::string& word) const {
-        return zeroPad(word);
-    }  
-private:
-    std::string zeroPad(const std::string& word) const {
-      return word + "000";
-    }
+   Soundex soundex;
 };
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
-    Soundex soundex;
-
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
     auto encoded = soundex.encode("A");
 
     ASSERT_THAT(encoded, Eq("A000"));
+};
 
-}
-
-TEST(SoundexEncoding, PadsWithZeroesToEnsureThreeDigits) {
-    Soundex soundex;
-
+TEST_F(SoundexEncoding, PadsWithZeroesToEnsureThreeDigits) {
     auto encoded = soundex.encode("I");
 
     ASSERT_THAT(encoded, Eq("I000"));
-}
+};
