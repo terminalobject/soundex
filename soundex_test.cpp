84d463274da9a797551d899bed7100f899ccf86c
terminalobject <doctorboredom@hotmail.com>
2017-10-16 19:56:36 +0100

    first tests, all in one file

--- soundex_test.cpp
+++ soundex_test.cpp
@@ -1,10 +1,31 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+using ::testing::Eq;
 
 class Soundex {
-    ;
+public:
+    std::string encode(const std::string& word) const {
+        return zeroPad(word);
+    }  
+private:
+    std::string zeroPad(const std::string& word) const {
+      return word + "000";
+    }
 };
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
     Soundex soundex;
+
+    auto encoded = soundex.encode("A");
+
+    ASSERT_THAT(encoded, Eq("A000"));
+
+}
+
+TEST(SoundexEncoding, PadsWithZeroesToEnsureThreeDigits) {
+    Soundex soundex;
+
+    auto encoded = soundex.encode("I");
+
+    ASSERT_THAT(encoded, Eq("I000"));
 }
