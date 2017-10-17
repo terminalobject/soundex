7253877c571508b72966c5ba54a75cfcb1f5c515
terminalobject <doctorboredom@hotmail.com>
2017-10-17 21:42:38 +0100

    rule 4 implementation

--- Soundex.h
+++ Soundex.h
@@ -2,17 +2,55 @@
 
 #define Soundex_h
 
+#include <string>
+#include <unordered_map>
+
+static const size_t MaxCodeLength = 4;  
+
+
 class Soundex
 {
 public:
-  std::string encode(const std::string& word) const {
-      return zeroPad(word);
+   std::string encode(const std::string& word) const {
+      return zeroPad(head(word) + encodedDigits(tail(word)));
+
   }
 
 private:
-    std::string zeroPad(const std::string& word) const {
-      return word + "000";
-    }
+   std::string head(const std::string& word) const {
+       return word.substr(0, 1);
+   }
+
+   std::string tail(const std::string& word) const {
+       return word.substr(1);
+   }
+   
+   std::string encodedDigits(const std::string& word) const {
+       std::string encoding;
+       for (auto letter: word) {
+           if (encoding.length() == MaxCodeLength - 1) break;
+           encoding += encodedDigit(letter); 
+       }
+       return encoding;
+   }
+
+   std::string encodedDigit(char letter) const {
+      const std::unordered_map<char, std::string> encodings {
+         {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+         {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"},
+         {'d', "3"}, {'t', "3"},
+         {'l', "4"},
+         {'m', "5"}, {'n', "5"},
+         {'r', "6"}
+      };
+      auto it = encodings.find(letter);
+      return it == encodings.end() ? "" : it->second;
+   }
+
+   std::string zeroPad(const std::string& word) const {
+     auto zerosNeeded = MaxCodeLength - word.length();
+     return word + std::string(zerosNeeded, '0');
+   }
 };
 
 #endif
--- soundex_test.cpp
+++ soundex_test.cpp
@@ -20,3 +20,21 @@ TEST_F(SoundexEncoding, PadsWithZeroesToEnsureThreeDigits) {
 
     ASSERT_THAT(encoded, Eq("I000"));
 };
+
+TEST_F(SoundexEncoding, ReplaceConsonantsWithAppropriateDigits) {
+   EXPECT_THAT(soundex.encode("Ab"), Eq("A100"));
+   EXPECT_THAT(soundex.encode("Ac"), Eq("A200"));
+   EXPECT_THAT(soundex.encode("Ad"), Eq("A300"));
+};
+
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics) {
+   ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+};
+
+TEST_F(SoundexEncoding, ReplaceMutipleConsonantsWithDigits) {
+   ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
+};
+
+TEST_F(SoundexEncoding, LimitsLengthToFourCharacters) {
+   ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
+}
